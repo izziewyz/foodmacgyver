b9f0b5a19018adeac6e9db7192e1da6b558ab907
Alicia T <Alis@PERLAs-MacBook-Pro.local>
2016-12-11 12:26:23 -0600

    newlibrary

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,9 +3,9 @@
 
 $("#Search").on("click", function(event) {
           event.preventDefault();
-
-          var ingredient =  $("#searchTerm").val().trim();
-          console.log(ingredient);
+          console.log(searchString);
+          // var ingredient =  $("#searchTerm").val().trim();
+          // console.log(ingredient);
 
           // event.preventDefault() can be used to prevent an event's default behavior.
           // Here, it prevents the submit button from trying to submit a form when clicked
@@ -25,11 +25,12 @@ document.addEventListener('keyup', function(event) {
 });      
 
  $.ajax({
-    url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + ingredient + "&limitLicense=false&number=5&ranking=2" , // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+    url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + searchString + "&limitLicense=false&number=5&ranking=2" , // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
     type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
     data: {}, // Additional parameters here
     dataType: 'json',
     success: function(data) {
+      console.log(searchString);
        console.log(data);  
 
        for(var i=0; i < data.length; i++){
@@ -60,7 +61,7 @@ document.addEventListener('keyup', function(event) {
         $(".imagebutton").on("click", function(event) {
           console.log($(this).attr("data2"));
 
-          
+
 
 
         });
