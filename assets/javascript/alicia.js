7226eb04721cec4a8c0e9ed2907dbf0ab989e0f1
Yanzhao Wang <izzie@yanzhaos-MacBook-Air.local>
2016-12-11 14:47:13 -0600

    ohSnapworks

--- assets/javascript/alicia.js
+++ assets/javascript/alicia.js
@@ -1,39 +1,26 @@
 
 
- //User press a key
+$('#add').on('click', function() { 
 
-// document.addEventListener('keyup', function(event) {
 
-//            var keyPress = event.keyCode;
+var newIngredient2 = $("#searchTerm").val();
+console.log(newIngredient2);
 
-//            //unless the key is a letter, the code will not run
-//            if (!(keyPress >= 65 && keyPress <= 120) && (keyPress != 32 && keyPress != 0)) {
-//                event.preventDefault();
-//                // function() { ohSnap('A notification with default options')};
-//                alert("Game only works with letter keys. No numbers or symbol keys");
-
-//            }
-// }); 
-
-// $('#searchTerm').on('keyup'),function(){
-// 	ohSnap('you need to type a letter !!');
-// };     
-function allLetter(inputtxt)  
-  {  
+			 
    var letters = /^[A-Za-z]+$/;  
-   if(inputtxt.value.match(letters))  
+   
+   if(newIngredient2.match(letters))  
      {  
       return true;  
      }  
    else  
      {  
-     alert("message");  
+     ohSnap(' you need to click a letter !!' , {color:'red'}) 
      return false;  
      }  
-  }  
-  
+    
 
-$('#add').on('click', function() { ohSnap(' you need to click a letter !!' , {color:'red'})});
+	});
 // $('#red').on('click', function() { ohSnap('Oh Snap! You can\'t access this page!', {'color':'red'})});
 // $('#green').on('click', function() { ohSnap('Ahh Yeah! Your account was created.', {'color':'green'})});
 // $('#blue').on('click', function() { ohSnap('We are closed right now. Come back tomorrow.', {'color':'blue'})});
